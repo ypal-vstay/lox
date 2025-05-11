9c644acf34897531b06e78f2015dc89e86a230eb
ypal-vstay <dimafigik@gmail.com>
2025-05-11 15:17:31 +0300

    Update Ping Pong.py

--- Ping Pong.py
+++ Ping Pong.py
@@ -1,12 +1,12 @@
 from pygame import *
-'''Необходимые классы'''
 
 
-#класс-родитель для спрайтов
+
+
 class GameSprite(sprite.Sprite):
    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
        super().__init__()
-       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.image = transform.scale(image.load(player_image), (wight, height)) 
        self.speed = player_speed
        self.rect = self.image.get_rect()
        self.rect.x = player_x
@@ -32,22 +32,22 @@ class Player(GameSprite):
            self.rect.y += self.speed
 
 
-#игровая сцена:
-back = (200, 255, 255) #цвет фона (background)
+
+back = (200, 255, 255) 
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 
-#флаги, отвечающие за состояние игры
+
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
 
 
-#создания мяча и ракетки   
+  
 racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
@@ -80,19 +80,19 @@ while game:
            speed_x *= -1
            speed_y *= 1
       
-       #если мяч достигает границ экрана, меняем направление его движения
+       
        if ball.rect.y > win_height-50 or ball.rect.y < 0:
            speed_y *= -1
 
 
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+       
        if ball.rect.x < 0:
            finish = True
            window.blit(lose1, (200, 200))
            game_over = True
 
 
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+       
        if ball.rect.x > win_width:
            finish = True
            window.blit(lose2, (200, 200))
